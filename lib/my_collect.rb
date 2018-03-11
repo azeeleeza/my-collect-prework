97965c8daeeaa29a9e906b23c38e978588ce9742
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-11 00:27:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i=0
   new_collection=[]
   while i < collection.length
-    yield(new_collection<<collectioin[i])
+    yield(collection<<collection[i])
     i+=1
   end
 end
